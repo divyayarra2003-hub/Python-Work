c614620f67ba15ff2a78634c01b58351be44f064
divyayarra2003-hub <divyayarra2003@gmail.com>
2026-08-31 09:35:14 +0530

    Variables

---
+++ day-2/variable_operations.py
@@ -0,0 +1,29 @@
+Python 3.13.14 (tags/v3.13.14:fd17997, Jun 10 2026, 13:03:48) [MSC v.1944 64 bit (AMD64)] on win32
+Enter "help" below or click "Help" above for more information.
+>>> a=10
+>>> A=20
+>>> a
+10
+>>> A
+20
+>>> a=b=c=10
+>>> a
+10
+>>> b
+10
+>>> c
+10
+>>> a=10
+>>> b=20
+>>> a,b=b,a
+>>> a
+20
+>>> b
+10
+>>> a,b,c=10,20,30
+>>> a
+10
+>>> b
+20
+>>> c
+30
