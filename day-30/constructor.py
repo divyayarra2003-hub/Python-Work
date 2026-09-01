5143730e7e0ca2e8d2a8fc2b41809603c79d1769
divyayarra2003-hub <divyayarra2003@gmail.com>
2026-09-01 16:46:12 +0530

    Removed day-10 to day-34

--- day-30/constructor.py
+++
@@ -1,8 +0,0 @@
-class Flipkart:
-    def __init__(self,name,phone):
-        self.name = name
-        self.phone = phone
-        print(f'Hello {self.name}, Welcome to the flipkart')
-
-divya = Flipkart('divya',908439434)
-sri = Flipkart('sri',98399894839)
--- day-32/polymorphism.py
+++
@@ -1,7 +0,0 @@
-'''
-it means one method different forms and it contains 
-method overloading : same method, different parameters, python doesn't have method overloading
-                     but we can do it by using default arguments
-method overriding  : same method in different classes
-operator overriding: generally operators work with arguments, here we work with objects instead of arguments
-'''
