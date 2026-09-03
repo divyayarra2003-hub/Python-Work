df241e73102482fea43566c15aa0f5453cb0170f
divyayarra2003-hub <divyayarra2003@gmail.com>
2026-09-03 13:50:39 +0530

    conditional statements

---
+++ day-11/simpleif.py
@@ -0,0 +1,14 @@
+'''
+sales = int(input("Enter the sales amount: "))
+if sales>1000:
+    print("Best Seller")
+'''
+'''
+eli_acc = eval(input("Eligible account: "))
+ver_sub = eval(input("Meta verified subscription: "))
+if eli_acc and ver_sub:
+    print("verified badge granted")'''
+
+status = eval(input("Enter the status:"))
+if status:
+    print("Extra rain charges applied")
